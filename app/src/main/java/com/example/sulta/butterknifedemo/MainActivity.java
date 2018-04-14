ca26b2cfd65b17abf5058734e99b1bcad364ba2f
engsulta <en.ahmedsultan@gmail.com>
2018-04-14 21:38:26 +0200

    butter knife button test

--- app/src/main/java/com/example/sulta/butterknifedemo/MainActivity.java
+++ app/src/main/java/com/example/sulta/butterknifedemo/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.sulta.butterknifedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.textView)
     TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
 
 
         mTextView.setText("Iam a butter knife  please  use me ...");
+
+
     }
+
+
+    @OnClick(R.id.main_btn) void onButtonClicked(View view){
+        switch (view.getId())
+        {
+            case R.id.main_btn:
+            {
+                Toast.makeText(this, "butter knife on a button tested", Toast.LENGTH_SHORT).show();
+            }
+            default:
+            {
+                //ToDo another action
+            }
+        }
+    }
+
+
+
 }
